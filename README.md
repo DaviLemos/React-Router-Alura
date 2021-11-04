6fcd539eb3a2d4d2a529f06f75a369a295ecb4ed
Davi Teixeira <davi@furg.br>
2021-11-04 18:44:33 -0300

    All Project

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# React-Router-Alura
- 
+Projeto proposto no curso da Alura [React Router: Navegação em uma SPA](https://www.alura.com.br/curso-online-react-router-navegacao-spa).
+
+### `npm start`
+
+Irá executar o projeto na url [http://localhost:3000](http://localhost:3000)
---
+++ src/paginas/SubCategoria.jsx
@@ -0,0 +1,9 @@
+import React, { useState } from 'react';
+import { useParams } from 'react-router';
+import ListaPost from '../components/ListaPost';
+
+const SubCategoria = ({}) => {
+  const { subcategoria } = useParams();
+  return <ListaPost url={`/posts?subcategoria=${subcategoria}`} />;
+};
+export default SubCategoria;
